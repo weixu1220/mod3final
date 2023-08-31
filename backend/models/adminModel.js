e345ee02256885eaacacb33ce79afa9979ccdd95
Vivian Rice <weixu1220@gmail.com>
2023-08-31 09:23:33 -0600

    admin/user model

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const adminSchema = new Schema({
-
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
 })
 const Admin = mongoose.model('Admin', adminSchema)
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const userSchema = new Schema({
 
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    firstname:{
+        type:String,
+        required: true,
+    },
+    lastname:{
+        type:String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    ads:{
+        type: Boolean,
+    },
+    agree:{
+        type: Boolean,
+        enum: [ true ]
+    }
 })
-const User = mongoose.model('user', userSchema)
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
